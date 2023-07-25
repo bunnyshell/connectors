c474376c7201ae113612f461a6eed970234a90c2
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-07-25 18:17:39 +0300

    Xata - Renamed target branch var

--- xata-dataplatform/scripts/delete_database.sh
+++ xata-dataplatform/scripts/delete_database.sh
@@ -1,12 +1,12 @@
-echo "Xata designated database branch is $TARGET_BRANCH_NAME"
+echo "Xata designated database branch is $TARGET_BRANCH"
 
 if [ -n "$BRANCH_ID" ]; then
-    echo "Checking to see if the database branch $TARGET_BRANCH_NAME ($BRANCH_ID) already exists..."
+    echo "Checking to see if the database branch $TARGET_BRANCH ($BRANCH_ID) already exists..."
 
     existing_branch_response=$( \
         curl -s -w "\n%{http_code}" \
                 --request GET \
-                --url https://$WORKSPACE.$REGION.xata.sh/db/$DB_NAME:$TARGET_BRANCH_NAME \
+                --url https://$WORKSPACE.$REGION.xata.sh/db/$DB_NAME:$TARGET_BRANCH \
                 --header 'accept: application/json' \
                 --header "authorization: Bearer $API_TOKEN" \
     )
@@ -14,9 +14,9 @@ if [ -n "$BRANCH_ID" ]; then
     existing_branch_response_body=$(echo "$existing_branch_response" | sed '$d')
 
     if [ "$existing_branch_response_code" -eq 200 ]; then
-        echo "Database branch $TARGET_BRANCH_NAME exists in Xata, ID $BRANCH_ID."
+        echo "Database branch $TARGET_BRANCH exists in Xata, ID $BRANCH_ID."
     elif [ "$existing_branch_response_code" -eq 404 ]; then
-        echo "Database branch $TARGET_BRANCH_NAME does not exist in Xata, nothing to delete."
+        echo "Database branch $TARGET_BRANCH does not exist in Xata, nothing to delete."
         exit 0
     else
         echo "HTTP Error: $code"
@@ -30,7 +30,7 @@ echo "Deleting the database branch..."
 delete_branch_response=$( \
     curl -s -w "\n%{http_code}" \
         --request DELETE \
-        --url https://$WORKSPACE.$REGION.xata.sh/db/$DB_NAME:$TARGET_BRANCH_NAME \
+        --url https://$WORKSPACE.$REGION.xata.sh/db/$DB_NAME:$TARGET_BRANCH \
         --header 'accept: application/json' \
         --header "authorization: Bearer $API_TOKEN" \
 )
@@ -38,9 +38,9 @@ delete_branch_response_code=$(echo "$delete_branch_response" | tail -n1)
 delete_branch_response_body=$(echo "$delete_branch_response" | sed '$d')
 
 if [ "$delete_branch_response_code" -eq 200 ]; then
-    echo "Successfully deleted database branch $TARGET_BRANCH_NAME."
+    echo "Successfully deleted database branch $TARGET_BRANCH."
 else
-    echo "Error deleting database branch $TARGET_BRANCH_NAME."
+    echo "Error deleting database branch $TARGET_BRANCH."
     echo "HTTP Error: $delete_branch_response_code"
     echo "$delete_branch_response_body"
     exit 1
